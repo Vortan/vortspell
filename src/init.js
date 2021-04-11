60f13b9a6e56669e37c9329ad94170c9d7344d9e
souren <souren@papazian.info>
2021-04-11 19:06:14 +0400

    Add hyspell dicts and use those

--- src/init.js
+++ src/init.js
@@ -1,25 +1,25 @@
-const nodehun = require('nodehun');
-const Promise = require('bluebird');
-const path = require('path')
-const fs = require('fs');
-const util = require('util')
+const nodehun = require("nodehun");
+const Promise = require("bluebird");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
 
-const dictionaryDir = path.join(path.dirname(fs.realpathSync(__filename)), '../dicts');
-var affbuf, dicbuf, dict
+const dictionaryDir = path.join(path.dirname(fs.realpathSync(__filename)), "../dicts");
+var affbuf, dicbuf, dict;
 
-module.exports = function(commander) {
-  return new Promise(function(resolve, reject) {
-    if (commander.custom) {
-      affbuf = fs.readFileSync(util.format('%s/%s.aff', dictionaryDir, commander.custom));
-      dicbuf = fs.readFileSync(util.format('%s/%s.dic', dictionaryDir, commander.custom));
-    } else if (commander.western) {
-      affbuf = fs.readFileSync(dictionaryDir+'/armenian_western.aff');
-      dicbuf = fs.readFileSync(dictionaryDir+'/armenian_western.dic');
-    } else {
-      affbuf = fs.readFileSync(dictionaryDir+'/armenian_eastern.aff');
-      dicbuf = fs.readFileSync(dictionaryDir+'/armenian_eastern.dic');
-    }
-    dict = new nodehun(affbuf, dicbuf)
-    resolve(dict);
-  });
+module.exports = function (commander) {
+    return new Promise(function (resolve, reject) {
+        if (commander.custom) {
+            affbuf = fs.readFileSync(util.format("%s/%s.aff", dictionaryDir, commander.custom));
+            dicbuf = fs.readFileSync(util.format("%s/%s.dic", dictionaryDir, commander.custom));
+        } else if (commander.western) {
+            affbuf = fs.readFileSync(dictionaryDir + "/hyspell-c.aff");
+            dicbuf = fs.readFileSync(dictionaryDir + "/hyspell-c.dic");
+        } else {
+            affbuf = fs.readFileSync(dictionaryDir + "/hyspell-r.aff");
+            dicbuf = fs.readFileSync(dictionaryDir + "/hyspell-r.dic");
+        }
+        dict = new nodehun(affbuf, dicbuf);
+        resolve(dict);
+    });
 };
